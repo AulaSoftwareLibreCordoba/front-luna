d9d0bccb14a024def14b9d637c59f428fc9c7436
Alvaro Fernandez <alvaroofernaandez@gmail.com>
2024-08-07 23:06:59 +0200

    botones y estilos actualizados

--- src/components/Denuncias.jsx
+++ src/components/Denuncias.jsx
@@ -114,15 +114,21 @@ const FormComponent = () => {
         </div>
       </div>
       <div className="flex justify-center">
-        <button
-          type="submit"
-          className="flex font-bold bg-gradient-to-b from-violet-950 to-transparent p-4 rounded-lg hover:bg-purple-900 text-white transition-colors duration-300 border-[1px] border-violet-900"
-        >
-          Enviar
-        </button>
+      <button type="submit" class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-white transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-violet-500 group">
+        <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-violet-600 group-hover:h-full"></span>
+        <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+        <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+        </span>
+        <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+        <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+        </span>
+        <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Enviar</span>
+      </button>
       </div>
     </form>
   );
 };
 
 export default FormComponent;
+
+
