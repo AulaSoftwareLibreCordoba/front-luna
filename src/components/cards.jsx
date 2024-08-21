e59c0e57698dabc4db428b359a576c4bbfed4a78
Fran <franbermejo.dam@gmail.com>
2024-08-22 01:31:00 +0200

    Cambio en el estilo de la pizarra

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Cards = ({ id, text }) => {
   return (
-    <div className="w-48 h-48 relative p-2 m-5 flex justify-center items-center text-white text-base shadow-2xl rotate-6">
-      <div className="w-3 h-3 bg-white absolute rounded-md translate-x-[-50%] top-[-7px] left-[50%] shadow-lg"></div>
-      <div className="w-full h-full text-base text-white overflow-hidden flex justify-center items-center text-center">
+    <div className="w-48 h-48 relative p-2 m-5 flex border border-violet-900 rounded justify-center items-center  text-base">
+      {/* <div className="w-3 h-3 bg-red-600 absolute rounded-md translate-x-[-50%] top-[-7px] left-[50%] shadow-lg"></div> */}
+      <div className="w-full h-full text-base texto-pizarra overflow-hidden flex justify-center items-center text-center">
         {text}
       </div>    
     </div>
