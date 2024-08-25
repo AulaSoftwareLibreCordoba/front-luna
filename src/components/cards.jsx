71bc5d251ac97c3de5768aa48eb55d2e7299059f
Fran <franbermejo.dam@gmail.com>
2024-08-26 00:33:36 +0200

    Cambio en el hover de las cards

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
+import React,{ useEffect, useState} from 'react';
 
 const Cards = ({ id, text, success }) => {
-  const cardStyle = { color: success ? 'rgb(34 197 94)' : 'rgb(185 28 28)' };
-
   return (
     <div className="w-48 h-56 relative p-2 m-5 border border-violet-900 rounded justify-center items-center text-base hover:scale-125 transition duration-300 grid">
       {/* <div className="w-3 h-3 bg-red-600 absolute rounded-md translate-x-[-50%] top-[-7px] left-[50%] shadow-lg"></div> */}
@@ -10,9 +8,9 @@ const Cards = ({ id, text, success }) => {
         {text?.length > 100 ? text.slice(0, 100) + "..." : text}
       </div>
       <hr className='w-3/4 mx-auto border-violet-900'></hr>
-      <span className='justify-end flex'>
-        <i className="fa-solid fa-heart mx-6" style={cardStyle}></i>
-      </span>
+      <button className='justify-end flex'>
+      <i className={`fa-solid fa-heart fa-lg mx-6 ${success ? 'text-red-600' : 'text-gray-400'} hover:text-red-400 duration-100`}></i>
+    </button>
     </div>
   );
 };
