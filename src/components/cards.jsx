a5361fa803bfa33f6d01f8543a205a2d9b8f399d
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-08-30 23:53:37 +0200

    responsive adaptado pizarra

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -14,7 +14,7 @@ const Cards = ({ id, text, success, nombre }) => {
 
   return (
     
-    <div className="w-80 h-80 sm:w-72 sm:mx-auto xl:w-80 xl:h-80 relative p-5 m-5 border border-violet-900 rounded items-center text-base hover:scale-110 transition duration-300 grid">
+    <div className="w-64 h-64 sm:w-72 sm:h-72 md:w-[19rem] md:h-[19rem] xl:w-[20rem] xl:h-[20rem] 2xl:w-[20rem] 2xl:h-[20rem] relative p-5 m-5 border mx-auto border-violet-900 rounded items-center text-base hover:scale-110 transition duration-300 grid">
       <div className="w-full h-full text-base texto-pizarra overflow-hidden grid items-center">
         <span className='text-center text-2xl'>{nombre}</span>
         <hr className='w-full mx-auto border-violet-900'></hr>
