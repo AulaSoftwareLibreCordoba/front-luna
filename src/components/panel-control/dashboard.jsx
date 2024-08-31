6b33e93b1521048eba218f360c570e6e01e4f02b
juan <jdeomoya@gmail.com>
2024-08-31 16:57:36 +0200

    Axios instalada

--- src/components/panel-control/dashboard.jsx
+++ src/components/panel-control/dashboard.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import edituser from "./edituser";
 // import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faSign } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
-import { faClipboard } from '@fortawesome/free-solid-svg-icons';
-import { faSignOut } from '@fortawesome/free-solid-svg-icons';
-import { faChartPie } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faSign } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { faClipboard } from "@fortawesome/free-solid-svg-icons";
+import { faSignOut } from "@fortawesome/free-solid-svg-icons";
+import { faChartPie } from "@fortawesome/free-solid-svg-icons";
 
 function Dashboard() {
   const [isDropdownVisible, setDropdownVisible] = useState(false);
@@ -21,16 +20,16 @@ function Dashboard() {
     <div className="flex flex-col bg-[#0f0c29] w-64 h-screen">
       <div className="flex flex-col items-center">
         <div className="text-center mt-5 p-10 text-white bg-[#0f0c29] w-12 h-12 flex items-center justify-center ease-in-out">
-            <div className="text-white">
-              <FontAwesomeIcon icon={faChartPie} className="size-12"/>
-            </div>
+          <div className="text-white">
+            <FontAwesomeIcon icon={faChartPie} className="size-12" />
+          </div>
         </div>
       </div>
-      <div className="my-auto mt-[15rem]">
+      <div className="my-auto ">
         <a href="/users">
           <div className="p-4 text-white hover:bg-[#252849] cursor-pointer">
             <div className="flex items-center gap-5 m-3 text-xl">
-              <FontAwesomeIcon icon={faUser} className="size-7"/>
+              <FontAwesomeIcon icon={faUser} className="size-7" />
               <p>Usuarios</p>
             </div>
           </div>
@@ -45,7 +44,7 @@ function Dashboard() {
         </a>
         <a href="/notas-dash">
           <div className="p-4 text-white hover:bg-[#252849] cursor-pointer">
-          <div className="flex items-center gap-5 m-3 text-xl">
+            <div className="flex items-center gap-5 m-3 text-xl">
               <FontAwesomeIcon icon={faClipboard} className="size-7" />
               <p>Notas pizarra</p>
             </div>
@@ -56,7 +55,7 @@ function Dashboard() {
         <div className="mx-auto text-center p-4 text-white  bg-red-800  hover:bg-red-900 rounded-full size-14 flex items-center justify-center transition-colors duration-300 ease-in-out">
           <a href="/">
             <div className="text-white hover:rounded-full cursor-pointer size-7">
-              <FontAwesomeIcon icon={faSignOut} className="size-7"/>
+              <FontAwesomeIcon icon={faSignOut} className="size-7" />
             </div>
           </a>
         </div>
