e2887496029e3c77eabe8270f11a4a283750d3d7
juan <jdeomoya@gmail.com>
2024-08-31 11:42:04 +0200

    logica de post en las denuncias. (Falla)

---
+++ node_modules/.vite/deps_temp_c02e2a6f/react-dom.js
@@ -0,0 +1,7 @@
+import {
+  require_react_dom
+} from "./chunk-T6U6IT7R.js";
+import "./chunk-256VXULK.js";
+import "./chunk-5WRI5ZAA.js";
+export default require_react_dom();
+//# sourceMappingURL=react-dom.js.map
--- src/components/Denuncias.jsx
+++ src/components/Denuncias.jsx
@@ -8,7 +8,7 @@ const FormComponent = () => {
 
   const handleReasonChange = (e) => {
     const text = e.target.value;
-    const words = text.split(/\s+/);
+    const words = text.trim().split(/\s+/);
     const wordCount = words.length;
 
     if (wordCount <= 300) {
@@ -17,23 +17,35 @@ const FormComponent = () => {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmitLogin = (e) => {
     e.preventDefault();
-    console.log({
-      name,
-      selectedClass,
-      reason,
-    });
 
-    // Redirige a la página especificada
-    window.location.assign("/pizarra");
+    // usamos el post registramos el headers el cual incluye el token y pasamos al body los valores de id...
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id: name,
+        resumen: reason,
+        fechaCreacion: selectedClass,
+        validado: '1',
+      })
+    };
+
+    fetch('http://5.196.26.7:8080/denuncias', requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        // Redirige a la página especificada después de recibir la respuesta
+        window.location.assign("/pizarra");
+      })
+      .catch(error => console.error('Error:', error));
   };
 
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmitLogin} 
       className="mt-4 w-auto md:w-[50%] max-w-screen-lg mx-auto p-6 bg-white bg-opacity-5 border-2 border-violet-500 shadow-md rounded-lg backdrop-blur-md"
-      required
     >
       <div className="mb-6">
         <label
@@ -74,9 +86,9 @@ const FormComponent = () => {
       </div>
       <div>
         <div>
-          <label for="birthdate" className="block text-gray-500 font-bold text-xl mb-2">Fecha de los hechos</label>
-          <input id="birthdate" type="date"  min="1900-01-01" max="2100-01-01" class="datepicker rounded-md w-full p-3 bg-transparent border-2 mb-3 border-violet-500 focus:outline-none focus:ring-2 focus:ring-violet-500 text-lg " name="fecha_nacimiento" required/>
-      </div>
+          <label htmlFor="birthdate" className="block text-gray-500 font-bold text-xl mb-2">Fecha de los hechos</label>
+          <input id="birthdate" type="date" min="1900-01-01" max="2100-01-01" className="datepicker rounded-md w-full p-3 bg-transparent border-2 mb-3 border-violet-500 focus:outline-none focus:ring-2 focus:ring-violet-500 text-lg" name="fecha_nacimiento" required />
+        </div>
       </div>
       <div className="mb-6">
         <label htmlFor="class" className="block text-gray-500 font-bold text-xl mb-2"> Letra </label>
@@ -116,21 +128,19 @@ const FormComponent = () => {
         </div>
       </div>
       <div className="flex justify-center">
-      <button type="submit" class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-white transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-violet-500 group">
-        <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-violet-600 group-hover:h-full"></span>
-        <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-        <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-        </span>
-        <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-        <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-        </span>
-        <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Enviar</span>
-      </button>
+        <button type="submit" className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-white transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-violet-500 group">
+          <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-violet-600 group-hover:h-full"></span>
+          <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+            <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+          </span>
+          <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+            <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+          </span>
+          <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">Enviar</span>
+        </button>
       </div>
     </form>
   );
 };
 
 export default FormComponent;
-
-
