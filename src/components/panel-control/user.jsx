3277a42a2cbd6cfb896d393fddc907be1603c946
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-09-04 12:51:43 +0200

    apartado usuarios terminado

--- src/components/panel-control/user.jsx
+++ src/components/panel-control/user.jsx
@@ -7,24 +7,26 @@ function User({ nombre, id, email }) {
   const backgroundColor = id % 2 === 0 ? "bg-white" : "bg-slate-200";
 
   return (
+    
     <div
-      className={`${backgroundColor} border border-inherit flex flex-col p-4 text-start box-border w-full mb-0 justify-between items-center xl:flex-row xl:text-center`}
+      className={`${backgroundColor} border border-inherit flex flex-col p-4 text-start box-border w-full mb-0 justify-between items-center lg:flex-row lg:text-center`}
     >
-      <div className="w-full flex flex-col xl:flex-row xl:w-3/4">
-        <div className="flex-1 xl:w-1/4">
-          <p className="font-bold md:hidden">Usuario: </p>
+    <div className="flex w-full">
+      <div className="w-full flex flex-col lg:flex-row lg:w-3/4 lg:items-center">
+        <div className="flex-1 lg:w-1/4">
+          <p className="font-bold lg:hidden">Usuario: </p>
           <p>{id}</p>
         </div>
-        <div className="flex-1 xl:w-1/4">
-          <p className="font-bold md:hidden">Nombre: </p>
+        <div className="flex-1 lg:w-1/4">
+          <p className="font-bold lg:hidden">Nombre: </p>
           <p>{nombre}</p>
         </div>
-        <div className="flex-1 xl:w-1/4">
-          <p className="font-bold md:hidden">Email: </p>
+        <div className="flex-1 lg:w-1/4">
+          <p className="font-bold lg:hidden">Email: </p>
           <p>{email}</p>
         </div>
       </div>
-      <div className="w-full xl:w-1/4 flex justify-between gap-2 mt-2 xl:mt-0 xl:justify-end">
+      <div className="w-full md:w-1/4 grid lg:flex justify-end gap-2 mt-2 lg:mt-0 lg:justify-end">
         <button
           id={`${id}`}
           className="grid justify-center items-center w-10 h-12 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-gradient-to-r from-[#f5935a] via-[#ee7d14] to-[#f36d00] hover:shadow-sm hover:shadow-orange-500 hover:scale-105 duration-300 hover:from-[#eb7a4e] hover:to-[#f0853d] xl:w-10"
@@ -35,6 +37,7 @@ function User({ nombre, id, email }) {
           <FontAwesomeIcon icon={faTrash} className="size-4" />
         </button>
       </div>
+    </div>
     </div>
   );
 }
